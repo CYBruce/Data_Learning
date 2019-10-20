fa754231a17cc155ccec498f6076bcab63464132
tt'ct'c'y田宸宇 <tiancy19@mails.tsinghua.edu.cn>
2019-10-20 09:07:47 +0800

    homework2 finish

--- homework3/naive_bayes.py
+++ homework3/naive_bayes.py
@@ -6,6 +6,7 @@ __date__   = "20191005"
 
 import numpy as np
 import pdb
+from sklearn.naive_bayes import BernoulliNB
 
 np.random.seed(2019)
 
@@ -29,14 +30,14 @@ class BernoulliNaiveBayes:
 
         # learn the p(y=1) and p(y=0)
         num_y1 = sum(y_train)
-        num_y0 = y_train.shape[0] - num_y1
-        self.p_y1 = num_y1 / y_train.shape[0]
+        num_y0 = len(y_train) - num_y1
+        self.p_y1 = num_y1 / len(y_train)
         self.p_y0 = 1 - self.p_y1
 
         # learn the p(x|y=0), p(x|y=1), they should be a vector with shape (# of feature, ).
         # Laplace smoothing
-        y1_set = x_train[np.where(y_train > 0)]
-        y0_set = x_train[np.where(y_train < 1)]
+        y1_set = x_train[np.where(y_train == 1)]
+        y0_set = x_train[np.where(y_train == 0)]
         self.p_x_y0 = (np.sum(y0_set, axis=0)+1)/(num_y0+2)
         self.p_x_y1 = (np.sum(y1_set, axis=0)+1)/(num_y1+2)
 
@@ -50,8 +51,8 @@ class BernoulliNaiveBayes:
             pred: np.array, shape (num_samples, )
         """
         x_test = x_test.astype(int)
-        p0 = np.sum(np.log(self.p_x_y0 * x_test + (1 - self.p_x_y0) * (1 - x_test)), axis=1)
-        p1 = np.sum(np.log(self.p_x_y1 * x_test + (1 - self.p_x_y1) * (1 - x_test)), axis=1)
+        p0 = np.sum(np.log(self.p_x_y0 * x_test + (1 - self.p_x_y0) * (1 - x_test)), axis=1) + np.log(self.p_y0)
+        p1 = np.sum(np.log(self.p_x_y1 * x_test + (1 - self.p_x_y1) * (1 - x_test)), axis=1) + np.log(self.p_y1)
         pred = np.where((p1 - p0) > 0, 1, 0)
 
         # do inference to obtain p(y=0|x) and p(y=1|x),
@@ -96,11 +97,13 @@ if __name__ == '__main__':
 
     acc_func = lambda x,y: 100 * (x == y).sum() / y.shape[0]
 
+    # clf = BernoulliNB()
+    # clf.fit(x_train, y_train)
     clf = BernoulliNaiveBayes()
 
     clf.train(x_train, y_train)
-
     pred = clf.predict(x_test)
+    # pred = clf.predict(x_test)
 
     test_acc = acc_func(pred, y_test)
     print("You model acquires Test Acc:{:.2f} %".format(test_acc))
